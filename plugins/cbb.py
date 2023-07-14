3b6bfcacffa77fead6c256365d2fc5810b20f3ae
Luffy <120936732+anime91eki@users.noreply.github.com>
2023-07-14 15:49:26 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/KWICBOTS/File-sharing-Bot'>Click here</a>\n○ Channel : @kwicbotupdates\n○ Support Group : @kwicbotupdates</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://graph.org/file/6a2506bdf72802406b9a2.mp4'>Click here</a>\n○ Channel : @guardians_Bot_Updates\n○ Support Group : @Anime_Chat_Guardians</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
